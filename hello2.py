1a41fdaeae7c0beef9a3167e8010c231b3a1f221
Suspious <59746897+Suspious@users.noreply.github.com>
2021-09-27 09:39:26 +0200

    new code

--- hello2.py
+++ hello2.py
@@ -1,3 +1,3 @@
-def hello():
-    print('hello funcion from my town')
-hello()
+def showwelcome(welcometo: str='town'):
+    print('hello funcion from my town'+ welcometo)
+showwelcome()
---
+++ multiplier-x.py
@@ -0,0 +1,5 @@
+j = int(input('Van welk getal wilt u de tafel zien?\n'))
+def jan():
+    for i in range(11):
+        print(i*j)
+jan()
